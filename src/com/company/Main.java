07b26fb4deba7c8a74f1627a1678d78f6d35943d
Hantao Zhong <moon991015@gmail.com>
2021-02-26 05:23:55 +0800

    Add

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,18 +5,22 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	    //file in
+        //file in
         Path currentPath = Paths.get("Problem",args[0]).toAbsolutePath();
         System.out.println(currentPath.toString());
         File myObj = new File(currentPath.toString());
         Scanner in = null;
+
+        //Initialize Cars
+        ArrayList<Car> cars = new ArrayList();
+        // Initialize street status
+        HashMap<String, Integer> roadStatus = new HashMap<>();
+
         try {
             in = new Scanner(myObj);
         } catch (FileNotFoundException e) {
@@ -24,14 +28,16 @@ public class Main {
         }
         //file in over
         int D = in.nextInt(), I = in.nextInt(), S = in.nextInt(), V = in.nextInt(), F = in.nextInt();
-        //System.out.println(S);
-
-        Integer[][] intersectionArray = new Integer[S+1][S+1]; //i -> j if there is road then =1
-        String[][] intersectionName = new String[S+1][S+1]; //street name
+        System.out.println(S);
+        Integer[][] intersectionArray = new Integer[S+1][S+1];
+        String[][] intersectionName = new String[S+1][S+1];
         for (int i = 0; i < S; i++) {
             int start = in.nextInt();
             int end = in.nextInt();
             String name = in.next();
+
+            roadStatus.put(name, 0);
+
             int length = in.nextInt();
             System.out.println(start + " " + end + " " + name + " " + length);
             intersectionArray[start][end] = 1;
@@ -40,32 +46,84 @@ public class Main {
         for (int i = 0; i < V; i++) {
             int pass = in.nextInt();
             System.out.print(pass);
+
+            //Set up streets info for each cars
+            String streets[] = new String[pass-1];
+
             for (int j = 0; j < pass; j++) {
                 String intersection = in.next();
+
+                // The first street is ignored
+                if (j != 0) {
+                    streets[j-1] = intersection;
+                }
+
                 System.out.println(intersection);
             }
+            cars.add(new Car(streets));
         }
         System.out.println(D + " " + I + " " + S + " " + V + " " + F);
 
+
+        /**
+         * 模拟路况 Steven
+         */
+        // Skip the first sec since nothing happen when sec = 0
+        for (int sec = 0; sec < D; sec++) {
+            for (Car car: cars) {
+                if (car.streets.length > 0) {
+                    roadStatus.put(car.streets[0], roadStatus.get(car.streets[0])+1);
+
+                    // Delete the first street
+                    car.streets = Arrays.copyOfRange(car.streets, 1, car.streets.length);
+                }
+
+            }
+        }
+
         ArrayList<String> resultList = new ArrayList<String>();
-        int count = 0;
+        int count = 0; //first line of out put
+        int redLightCycle = 10;
         for (int j = 0; j <= S; j++) {
             ArrayList<String> tempStreetList = new ArrayList<String>();
             for (int i = 0; i <= S; i++) {
                 //System.out.println(i);
-                if ((intersectionArray[i][j]!=null)&&(intersectionArray[i][j] == 1)){ //if there is road between them
+                if ((intersectionArray[i][j]!=null)&&(intersectionArray[i][j] == 1)){
                     tempStreetList.add(intersectionName[i][j]);
                 }
             }
-            if (tempStreetList.size() > 0){ //if there is any road to j
-                //add into resultList in terms of output layout
+
+            if (tempStreetList.size() == 1){
+                //always green
                 count++;
                 resultList.add(Integer.toString(j));
                 resultList.add(Integer.toString(tempStreetList.size()));
                 for (String s : tempStreetList) {
-                    resultList.add(s + " 1");
-                    //resultList.add(s +" " + Integer.toString(D));
+                    resultList.add(s + " " + Integer.toString(D));
                 }
+            } else if (tempStreetList.size() > 1){
+                count++;
+                resultList.add(Integer.toString(j));
+                resultList.add(Integer.toString(tempStreetList.size()));
+
+                int sumOfTotalPossibleTraffic = 0;
+                for (String s : tempStreetList) {
+                    sumOfTotalPossibleTraffic += roadStatus.get(s);
+                }
+                float weight = 10 / sumOfTotalPossibleTraffic;
+
+                int checkSum = 0;
+                //Allocate cycle
+                for (String s : tempStreetList) {
+                    int totalTrafficOnTheRoad = roadStatus.get(s);
+                    int time = (int) weight * totalTrafficOnTheRoad;
+                    checkSum +=time;
+                    resultList.add(s + " " + Integer.toString(D));
+                }
+
+                 if (checkSum != 10){
+                     System.out.println("ERROR");
+                 }
             }
         }
 
@@ -75,6 +133,9 @@ public class Main {
 //            System.out.println(s);
 //        }
         printResultToFile(count,resultList,args[0]);
+
+
+        //
 
     }
 

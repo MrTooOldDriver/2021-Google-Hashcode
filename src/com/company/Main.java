feb8fad365c506ebbc1b575b3f5fcec4ea7a061e
Hantao Zhong <moon991015@gmail.com>
2021-02-26 02:44:16 +0800

    Add problem statement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,21 +9,34 @@ public class Main {
 
     public static void main(String[] args) {
 	    //file in
+        Path currentPath = Paths.get("Problem",args[0]).toAbsolutePath();
+        System.out.println(currentPath.toString());
+        File myObj = new File(currentPath.toString());
+        Scanner in = null;
         try {
-            Path currentPath = Paths.get("Problem",args[0]).toAbsolutePath();
-            System.out.println(currentPath.toString());
-            File myObj = new File(currentPath.toString());
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
-            }
-            myReader.close();
+            in = new Scanner(myObj);
         } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
             e.printStackTrace();
         }
-        
+        int D = in.nextInt(), I = in.nextInt(), S = in.nextInt(), V = in.nextInt(), F = in.nextInt();
+        for (int i = 0; i < S; i++) {
+            int start = in.nextInt();
+            int end = in.nextInt();
+            String name = in.next();
+            int length = in.nextInt();
+            System.out.println(start + " " + end + " " + name + " " + length);
+        }
+        for (int i = 0; i < V; i++) {
+            int pass = in.nextInt();
+            System.out.print(pass);
+            for (int j = 0; j < pass; j++) {
+                String intersection = in.next();
+                System.out.println(intersection);
+            }
+        }
+        System.out.println(D + " " + I + " " + S + " " + V + " " + F);
+
+
     }
 
 

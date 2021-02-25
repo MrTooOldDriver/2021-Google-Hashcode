bb46ac0f9b1c9ed1d74677ad3bb02709f7a5fcc9
Hantao Zhong <moon991015@gmail.com>
2021-02-26 03:32:00 +0800

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -18,13 +20,18 @@ public class Main {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        //file in over
         int D = in.nextInt(), I = in.nextInt(), S = in.nextInt(), V = in.nextInt(), F = in.nextInt();
+        Integer[][] intersectionArray = new Integer[S+1][S+1];
+        String[][] intersectionName = new String[S+1][S+1];
         for (int i = 0; i < S; i++) {
             int start = in.nextInt();
             int end = in.nextInt();
             String name = in.next();
             int length = in.nextInt();
             System.out.println(start + " " + end + " " + name + " " + length);
+            intersectionArray[start][end] = 1;
+            intersectionName[start][end] = name;
         }
         for (int i = 0; i < V; i++) {
             int pass = in.nextInt();
@@ -36,6 +43,31 @@ public class Main {
         }
         System.out.println(D + " " + I + " " + S + " " + V + " " + F);
 
+        ArrayList<String> resultList = new ArrayList<String>();
+        int count = 0;
+        for (int j = 0; j <= S; j++) {
+            ArrayList<String> tempStreetList = new ArrayList<String>();
+            for (int i = 0; i <= S; i++) {
+                //System.out.println(i);
+                if ((intersectionArray[i][j]!=null)&&(intersectionArray[i][j] == 1)){
+                    tempStreetList.add(intersectionName[i][j]);
+                }
+            }
+            if (tempStreetList.size() > 0){
+                count++;
+                resultList.add(Integer.toString(j));
+                resultList.add(Integer.toString(tempStreetList.size()));
+                for (String s : tempStreetList) {
+                    resultList.add(s + " 1");
+                }
+            }
+        }
+
+        System.out.println("test code start");
+        System.out.println(count);
+        for (String s : resultList) {
+            System.out.println(s);
+        }
 
     }
 
